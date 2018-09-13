6c671fd0affe9a04ff0e5c5ab4012f943a81cd55
WTL <shaggy_sg@yahoo.com.sg>
2018-09-14 01:42:18 +0800

    Fixed form default values after reset.

--- src/app/components/lp-form.component.ts
+++ src/app/components/lp-form.component.ts
@@ -3,6 +3,15 @@ import { NgForm } from '@angular/forms';
 
 import { AlbumItem } from '../app.component';
 
+export class myFormData {
+  constructor(
+    public albumName: string,
+    public artistName: string,
+    public albumRating: number,
+    public albumCoverImage: string
+  ) { }
+}
+
 @Component({
   selector: 'app-lp-form',
   templateUrl: './lp-form.component.html',
@@ -13,11 +22,17 @@ export class LpFormComponent implements OnInit {
   @Output()
   itemFromForm = new EventEmitter<AlbumItem>();
 
-  value = 5;
+  myData = new myFormData("Power of Love", "Celine Dion", 5, "https://vignette.wikia.nocookie.net/myat40/images/9/92/Celine_Dion_The_Power_Of_Love_cover.jpg");
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  resetToDefaultForm(myForm: NgForm) {
+    console.log('LpForm resetToDefaultForm: ', myForm.value);
+    myForm.resetForm(new myFormData("Power of Love", "Celine Dion", 5, "https://vignette.wikia.nocookie.net/myat40/images/9/92/Celine_Dion_The_Power_Of_Love_cover.jpg"));
+    return false;
   }
 
   processForm(myForm: NgForm) {
@@ -34,6 +49,19 @@ export class LpFormComponent implements OnInit {
     var toProcess: AlbumItem = { album_name: myForm.value.albumName, artist_name: myForm.value.artistName, album_rating: myForm.value.albumRating, image_url: myForm.value.albumCoverImage };
     console.log('LpForm toProcess: ', toProcess);
     this.itemFromForm.next(toProcess);
+
+    myForm.resetForm(new myFormData("Power of Love", "Celine Dion", 5, "https://vignette.wikia.nocookie.net/myat40/images/9/92/Celine_Dion_The_Power_Of_Love_cover.jpg"));
+
+    /*
     myForm.reset();
+    this.myData = new myFormData("Power of Love", "Celine Dion", 5, "http://example.com/power_of_love.png")
+    */
+
+    /*
+    this.myData.albumName = "Power of Love";
+    this.myData.artistName = "Celine Dion";
+    this.myData.albumRating = 5;
+    this.myData.albumCoverImage = "http://example.com/power_of_love.png";
+    */
   }
 }
